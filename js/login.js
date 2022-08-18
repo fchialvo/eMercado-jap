918659b8ca9c01279d4dc239509383f565e635ed
fchialvo <francochialvo03@gmail.com>
2022-08-18 15:01:54 -0300

    Entrega 1
    Agregados: Pantalla de autenticación y listado de productos obtenido mediante petición web.

---
+++ js/login.js
@@ -0,0 +1,20 @@
+
+(function () {
+    const form = document.querySelector('.needs-validation');
+
+    form.addEventListener('submit', function(event) {
+     event.preventDefault();
+
+     if (form.checkValidity() === false) {
+       form.classList.add('was-validated');
+     } else {
+       location.href = ("home.html");
+     }
+    }, false);
+    })()
+    
+    
+
+
+  
+
---
+++ js/products.js
@@ -0,0 +1,34 @@
+const URL = "https://japceibal.github.io/emercado-api/cats_products/101.json";
+
+function getHTML(product){
+    return `
+    <div class="list-group-item list-group-item-action cursor-active">
+            <div class="row">
+                <div class="col-3">
+                    <img src="${product.image}" alt="${product.name}" class="img-thumbnail">
+                </div>
+                <div class="col">
+                    <div class="d-flex w-100 justify-content-between">
+                        <h4 class="mb-1">${product.name}</h4>
+                        <small class="text-muted">${product.soldCount} vendidos</small>
+                    </div>
+                    <p class="mb-1">${product.description}</p>
+                </div>
+            </div>
+    </div>
+    `;
+}
+
+document.addEventListener('DOMContentLoaded', async function() {
+    const list = document.querySelector('#product-list-container');
+
+    const productList = await getJSONData(URL);
+
+    console.log(productList.data.products)
+    
+    for(let product of productList.data.products){
+        list.innerHTML += getHTML(product)
+    }
+})
+
+
