4fd83c283eaa853fd46eab4109247cd3e5636f01
fchialvo <francochialvo03@gmail.com>
2022-10-05 17:32:15 -0300

    Cambio en el diseño del login y productInfo

--- js/product-info.js
+++ js/product-info.js
@@ -3,91 +3,109 @@ const URL = "";
 function getHTML(product) {
   return `
     <div class="row">
-        <div class="col py-3"><h2>${product.name}</h2>
+    <div class = "border col-6">
+        <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="true">
+          <div class="carousel-indicators">
+            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="3" aria-label="Slide 4"></button>
+          </div>
+          <div class="carousel-inner">
+            <div class="carousel-item active">
+              <img src="${product.images[0]}" class="d-block w-100" alt="...">
+            </div>
+            <div class="carousel-item">
+              <img src="${product.images[1]}" class="d-block w-100" alt="...">
+            </div>
+            <div class="carousel-item">
+              <img src="${product.images[2]}" class="d-block w-100" alt="...">
+            </div>
+            <div class="carousel-item">
+              <img src="${product.images[3]}" class="d-block w-100" alt="...">
+            </div>
+          </div>
+          <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Previous</span>
+          </button>
+          <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Next</span>
+          </button>
         </div>
-        <hr>
-        <b>Precio</b><br>
-        <p>${product.currency} ${product.cost}</p><br>
-        <b>Descripción</b><br>
-        <p>${product.description}</p><br>
-        <b>Categoría</b><br>
-        <p>${product.category}</p><br>
-        <b>Cantidad de vendidos</b><br>
-        <p>${product.soldCount}</p><br>
-        <b class = "pb-2">Imágenes ilustrativas</b><br>
-        <div class="col-3"><img src="${product.images[0]}" width = "270px" alt=""></div>
-        <div class="col-3"><img src="${product.images[1]}" width = "270px" alt=""></div>
-        <div class="col-3"><img src="${product.images[2]}" width = "270px" alt=""></div>
-        <div class="col-3"><img src="${product.images[3]}" width = "270px" alt=""></div>
-    </div>
-    `;
+      </div>
+      <div class = "col-6">
+      <h2 class = "fs-2 fw-semibold">${product.name}</h2>
+      <hr>
+      <h3>${product.currency} ${product.cost}</h3>
+      <p class = "fs-5">${product.description}</p>
+      <b>Categoría</b>
+      <p class = "fs-6">${product.category}</p>
+      <p class = "fs-6"><b>${product.soldCount} </b>vendidos</p>
+      </div>     
+  </div>
+  `;
 }
 
 function getCommentsHTML(product) {
   return `
-    <div class="card card-white post ps-2 pt-2">  
-        <div class="post-heading">
-            <div class="float-left meta">
-                <div class="title h6">
-                    <b>${product.user}</b>
-                    - ${product.dateTime} -
-                    <div> 
-                      ${showStars(product.score)}
-                    </div>
-                </div>
-                </div>
-            </div>
-            <div class="post-description">
-                <p>${product.description}</p>
-            </div>
-    </div>  
-    `;
+  <div class="card m-2" style="width: 18rem;">
+    <div class="card-body">
+      <h5 class="card-title">${product.user}</h5>
+      <h6 class="card-subtitle mb-2 text-muted">${product.dateTime}</h6>
+      <p class="card-text">${product.description}</p>
+      <div> 
+      ${showStars(product.score)}
+      </div>
+    </div>
+  </div>
+  `;
 }
 
-function getRelatedProductsHTML(product){
+function getRelatedProductsHTML(product) {
   return `
   <div class = "row">
-  <div class="card mx-2 cursor-active" style="width: 18rem;" onclick="setProductID(${product.relatedProducts[0].id})">
-          <img src="${product.relatedProducts[0].image}" class="card-img-top" alt="${product.relatedProducts[0].name}">
-          <div class="card-body">
-            <h5 class="card-title">${product.relatedProducts[0].name}</h5>
-          </div>
+    <div class="card mx-2 cursor-active" style="width: 18rem;" onclick="setProductID(${product.relatedProducts[0].id})">
+            <img src="${product.relatedProducts[0].image}" class="card-img-top" alt="${product.relatedProducts[0].name}">
+            <div class="card-body">
+              <h5 class="card-title">${product.relatedProducts[0].name}</h5>
+            </div>
+    </div>
+    <div class="card cursor-active" style="width: 18rem" onclick="setProductID(${product.relatedProducts[1].id})">
+            <img src="${product.relatedProducts[1].image}" class="card-img-top" alt="${product.relatedProducts[1].name}">
+            <div class="card-body">
+              <h5 class="card-title">${product.relatedProducts[1].name}</h5>
+            </div>
+    </div>
   </div>
-  <div class="card cursor-active" style="width: 18rem" onclick="setProductID(${product.relatedProducts[1].id})">
-          <img src="${product.relatedProducts[1].image}" class="card-img-top" alt="${product.relatedProducts[1].name}">
-          <div class="card-body">
-            <h5 class="card-title">${product.relatedProducts[1].name}</h5>
-          </div>
-  </div>
-  </div>
-  `
+  `;
 }
 
-function showStars (puntaje) {
+function showStars(puntaje) {
   const star = '<span class="fa fa-star"></span>';
   const starChecked = '<span class="fa fa-star checked"></span>';
   let completos = Math.floor(puntaje);
-  let estrellas ='';
+  let estrellas = "";
   for (let i = 0; i < completos; i++) {
-  estrellas += starChecked      
+    estrellas += starChecked;
   }
 
   if (completos < 10) {
-      for (let i = completos; i < 5; i++) {
-         estrellas += star   
- } 
-  return estrellas
-}
+    for (let i = completos; i < 5; i++) {
+      estrellas += star;
+    }
+    return estrellas;
+  }
 }
 document.addEventListener("DOMContentLoaded", async function () {
   //Genero el html con la info del producto seleccionado, obtenida del URL que varía según el producto.
   const list = document.querySelector("#product-container");
   const producto = localStorage.getItem("productID");
   const URL = PRODUCT_INFO_URL + producto + EXT_TYPE;
-  console.log(producto)
+  console.log(producto);
   const productInfo = await getJSONData(URL);
   list.innerHTML += getHTML(productInfo.data);
-
 
   //Sección de comentarios
   const comments = document.querySelector("#comments");
@@ -102,32 +120,27 @@ document.addEventListener("DOMContentLoaded", async function () {
   relacionados.innerHTML += getRelatedProductsHTML(productInfo.data);
 });
 
-const btnEnviar = document.getElementById("enviar")
+const btnEnviar = document.getElementById("enviar");
 const userComment = document.getElementById("userComment");
-const select = document.getElementById("userScore")
-btnEnviar.addEventListener("click", ()=>{
+const select = document.getElementById("userScore");
+btnEnviar.addEventListener("click", () => {
   let selectedOption = select.options[select.selectedIndex].value;
   let hoy = new Date();
   // obtener la fecha
-  let fecha = hoy.toISOString().slice(0,10);
+  let fecha = hoy.toISOString().slice(0, 10);
   //obtener la hora
   let hora = hoy.toLocaleTimeString();
   const comments = document.querySelector("#comments");
-  comments.innerHTML += 
-  `<div class="card card-white post ps-2 pt-2">  
-      <div class="post-heading">
-          <div class="float-left meta">
-              <div class="title h6">
-                  <b>${localStorage.getItem("usuario")}</b>- ${fecha} - ${hora}
-                <div> 
-                  ${showStars(selectedOption)}
-                </div>
-              </div>
-            </div>
-        </div>
-        <div class="post-description">
-          <p>${userComment.value}</p>
-        </div>
-    </div> `
-})
-
+  comments.innerHTML += `
+  <div class="card m-2" style="width: 18rem;">
+    <div class="card-body">
+      <h5 class="card-title">${localStorage.getItem("usuario")}</h5>
+      <h6 class="card-subtitle mb-2 text-muted"> ${fecha}  ${hora}</h6>
+      <p class="card-text">${userComment.value}</p>
+      <div> 
+      ${showStars(selectedOption)}
+      </div>
+    </div>
+  </div>
+   `;
+});
